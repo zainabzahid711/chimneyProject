084bcb2b101e4da07d57fab6fb6ec39aa8862638
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2024-07-11 12:11:05 +0500

    useless files

--- src/components/input/Input.js
+++
@@ -1,6 +0,0 @@
-import style from "./Input.module.css";
-import Button from "@mui/material/Button";
-function Input() {
-  return <Button variant="contained">Chimney</Button>;
-}
-export default Input;
